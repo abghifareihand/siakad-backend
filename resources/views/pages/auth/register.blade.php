cf02840a02c32262a0e16f825e709080126c7310
abghifareihand <info.abghifareihan@gmail.com>
2024-03-15 17:56:52 +0700

    add fortify

---
+++ resources/views/pages/auth/register.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.auth')
+
+@section('title', 'Register')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/selectric/public/selectric.css') }}">
+@endpush
+
+@section('main')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Register</h4>
+        </div>
+
+        <div class="card-body">
+            <form method="POST">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input id="name" type="name" class="form-control" name="name" tabindex="1" required
+                        autofocus>
+                    <div class="invalid-feedback">
+                        Please fill in your name
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required
+                        autofocus>
+                    <div class="invalid-feedback">
+                        Please fill in your email
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="d-block">
+                        <label for="password" class="control-label">Password</label>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                        please fill in your password
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+    <script src="{{ asset('library/selectric/public/jquery.selectric.min.js') }}"></script>
+    <script src="{{ asset('library/jquery.pwstrength/jquery.pwstrength.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('js/page/auth-register.js') }}"></script>
+@endpush
