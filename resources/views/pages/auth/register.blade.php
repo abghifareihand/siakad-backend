965e19724f8bfb85a829bd6285939e154a2ccddb
abghifareihand <info.abghifareihan@gmail.com>
2024-03-15 18:38:39 +0700

    add register admin dashboard

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -15,33 +15,65 @@
 
         <div class="card-body">
             <form method="POST">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input id="name" type="name" class="form-control" name="name" tabindex="1" required
-                        autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your name
+                @csrf
+                <div class="row">
+                    <div class="form-group col-12">
+                        <label for="name">Name</label>
+                        <input id="name" type="text"
+                            class="form-control @error('name')
+                            is-invalid
+                        @enderror"
+                            name="name" autofocus>
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
+
                 </div>
 
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required
-                        autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your email
+                    <input id="email" type="email"
+                        class="form-control @error('email')
+                        is-invalid
+                    @enderror"
+                        name="email">
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="row">
+                    <div class="form-group col-12">
+                        <label for="password" class="d-block">Password</label>
+                        <input id="password" type="password"
+                            class="form-control @error('password')
+                            is-invalid
+                        @enderror"
+                            name="password">
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="form-group col-12">
+                        <label for="password2" class="d-block">Password Confirmation</label>
+                        <input id="password2" type="password" class="form-control" name="password_confirmation">
                     </div>
                 </div>
 
-                <div class="form-group">
-                    <div class="d-block">
-                        <label for="password" class="control-label">Password</label>
-                    </div>
-                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
-                    <div class="invalid-feedback">
-                        please fill in your password
-                    </div>
-                </div>
+
+
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-lg btn-block">
                         Register
